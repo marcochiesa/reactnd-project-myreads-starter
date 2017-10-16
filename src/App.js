c8816ab5ca9e242c5f5b3f6aed85f1883f8556da
Marco Chiesa <getmarcochiesa@gmail.com>
2017-10-16 03:26:39 -0500

    refactor: Extract search page to component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import DisplayShelf from './DisplayShelf'
+import DisplaySearch from './DisplaySearch'
 
 class BooksApp extends React.Component {
   state = {
@@ -36,29 +37,10 @@ class BooksApp extends React.Component {
 	  ];
     return (
       <div className="app">
-        <Route path='/search' render={({ history }) => (
-          <div className="search-books">
-            <div className="search-books-bar">
-			  <Link to="/" className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-					        )}/>
-			<Route exact path='/' render={() => (
+        <Route path='/search' render={() => (
+			<DisplaySearch shelves={shelves} onMoveBook={this.onMoveBook}/>
+        )}/>
+		<Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -74,7 +56,7 @@ class BooksApp extends React.Component {
 				<Link to="/search">Add a book</Link>
             </div>
           </div>
-				)}/>
+		)}/>
       </div>
     )
   }
